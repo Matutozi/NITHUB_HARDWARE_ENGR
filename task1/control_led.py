5d9652d2c4c34b5788bbb8c2823aa39bc7336b00
matutozi <sobowalegz2@gmail.com>
2025-02-18 13:32:50 +0100

    Task 1: controlling led with light dependent resistor

---
+++ task1/control_led.py
@@ -0,0 +1,27 @@
+"""
+Python Script that controls and LED using the ldr
+"""
+
+from machine import Pin, ADC
+from time import sleep
+
+ldr_pin = 26
+led_pin = 15
+
+ldr = ADC(Pin(ldr_pin))
+led = Pin(led_pin, Pin.OUT)
+
+print("Smart Home System")
+
+while True:
+    
+    light_level = ldr.read_u16()
+    
+    if light_level < 1000:
+        led.value(1) 
+        print("LED ON")
+    else:
+        led.value(0)
+        print("Idle, Led OFF")
+    sleep(0.1)
+
